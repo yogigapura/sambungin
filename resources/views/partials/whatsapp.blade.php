fbfcd69f9155fdc09b22eb9bbb218ec1d171508c
yogigapura <yogi.gapura@gmail.com>
2022-05-16 22:09:46 +0700

    memperbaiki penulisan nomer HP di blade whatsapp

--- resources/views/partials/whatsapp.blade.php
+++ resources/views/partials/whatsapp.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-<a href="https://api.whatsapp.com/send?phone=08986907020" target="_blank">
+<a href="https://api.whatsapp.com/send?phone=628986907020" target="_blank">
     <button class="btn-floating whatsapp">
        <img src="img/wa.png" alt="whatsapp">
     </button>
