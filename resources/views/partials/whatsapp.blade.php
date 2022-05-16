f813eb1bfe25400368ab64439dc2044d4de115ba
yogigapura <yogi.gapura@gmail.com>
2022-05-16 22:14:33 +0700

    menambahkan inisial text di blade whatsapp

--- resources/views/partials/whatsapp.blade.php
+++ resources/views/partials/whatsapp.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-<a href="https://api.whatsapp.com/send?phone=628986907020" target="_blank">
+<a href="https://api.whatsapp.com/send?phone=628986907020&text=Hi%20Sambungin" target="_blank">
     <button class="btn-floating whatsapp">
        <img src="img/wa.png" alt="whatsapp">
     </button>
